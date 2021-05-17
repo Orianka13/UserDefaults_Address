9dfdd880ede8776c12db442ffd6ff313d8dbde22
Olesia <odobrogor@gmail.com>
2021-05-17 16:10:29 +0300

    TableView and addressSaving form

--- UserDefaults_Address/ViewController.swift
+++ UserDefaults_Address/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct KeysDefaults {
+    static let keyCity = "city"
+    static let keyStreet = "street"
+    static let keyHouse = "house"
+    static let keyBuild = "build"
+    static let keyFlat = "flat"
+}
 
+class ViewController: UIViewController {
+    
+    let defaults = UserDefaults.standard
+
+    @IBOutlet weak var cityField: UITextField!
+    @IBOutlet weak var streetField: UITextField!
+    @IBOutlet weak var houseField: UITextField!
+    @IBOutlet weak var buildField: UITextField!
+    @IBOutlet weak var flatField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        cityField.text = defaults.string(forKey: KeysDefaults.keyCity)
+        streetField.text = defaults.string(forKey: KeysDefaults.keyStreet)
+        houseField.text = defaults.string(forKey: KeysDefaults.keyHouse)
+        buildField.text = defaults.string(forKey: KeysDefaults.keyBuild)
+        flatField.text = defaults.string(forKey: KeysDefaults.keyFlat)
+        
     }
 
-
+    @IBAction func saveAction(_ sender: UIButton) {
+        
+        let city = cityField.text!
+        let street = streetField.text!
+        let house = houseField.text!
+        let build = buildField.text!
+        let flat = flatField.text!
+        
+        if !city.isEmpty && !street.isEmpty && !house.isEmpty && !flat.isEmpty {
+            defaults.set(city, forKey: KeysDefaults.keyCity)
+            defaults.set(street, forKey: KeysDefaults.keyStreet)
+            defaults.set(house, forKey: KeysDefaults.keyHouse)
+            defaults.set(build, forKey: KeysDefaults.keyBuild)
+            defaults.set(flat, forKey: KeysDefaults.keyFlat)
+        }
+        
+    }
+    
 }
 
